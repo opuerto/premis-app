47ccf3f2fca5bf70dbe5005595319a89b6f60b9c
opuerto <oscar.puertohn@gmail.com>
2018-10-15 00:43:27 +0000

    including the stroke

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import Nav from './Nav';
-const Header = (props) => {
-    return (
-        <section className="Header">
-            <Nav />
-            <figure className="arrow-down">
-                <img src="assets/svg/down-arrow.svg" width={50} height={40} alt="arrow-down"/>
-            </figure>
-        </section>
-    );
-}
+import React from "react";
+import Nav from "./Nav";
+const Header = props => {
+  return (
+    <div>
+      <section className="Header">
+        <Nav />
+        <figure className="arrow-down">
+          <img
+            src="assets/svg/down-arrow.svg"
+            width={50}
+            height={40}
+            alt="arrow-down"
+          />
+        </figure>
+      </section>
+      <div className="stroke">
+        <img src="/assets/stroke.png" alt=""/>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
